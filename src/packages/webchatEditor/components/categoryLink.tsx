8d394777fc0ae07a58a753a80e8c83f916f10665
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-09-25 15:11:48 -0700

    bug fix: added function to parse Category from it's corresponding enum String

--- src/packages/webchatEditor/components/categoryLink.tsx
+++ src/packages/webchatEditor/components/categoryLink.tsx
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import { Dispatch, AnyAction } from 'redux';
 import { IAppState } from "../../Redux/reduxState";
 import { genericSingleAction, actionTypes } from "../../Redux/actions";
+import { Category } from "../../utilities/types";
 
 interface CategoryLinkProps {
     name: string;
@@ -12,9 +13,21 @@ interface CategoryLinkProps {
 
 export const CategoryLink = (props: CategoryLinkProps) => {
     const {updateRootStateVariable, name } = props;
+
+    
+    const getCategory = (stringName: string) : Category => {
+        // finds corresponding category entry (as tuple) from Category enum 
+        const categoryEntry: [string, Category] | undefined = Object.entries(Category).find(x => x[0] === stringName);
+
+        const category: Category = categoryEntry![1];
+        return category;
+    };
+
+    const displayName: Category = getCategory(name);
+    
     return (
-        <div onClick={() => {updateRootStateVariable('activeCategory', name)}}>
-            {name}
+        <div onClick={() => {updateRootStateVariable('activeCategory', displayName)}}>
+            {displayName}
         </div>
     );
 }
