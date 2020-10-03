399c02d452565052bad897a1fb706daf4e9f9f74
seth-way <seth@Sethaniels-MacBook-Pro.local>
2020-10-02 22:26:22 -0700

    simplified subCategory element, removed bug

--- src/packages/webchatEditor/components/customizationEntrySubCategory.tsx
+++ src/packages/webchatEditor/components/customizationEntrySubCategory.tsx
@@ -7,14 +7,32 @@ import { CustomizationEntry, WebChatStyleOption } from '../../Redux/reduxState';
 import { CustomizationEntrySelector } from './customizationEntrySelector';
 import { mergeStyles } from '@fluentui/react';
 import { useEffect, useState } from 'react';
-import { Collapse } from 'react-bootstrap';
+import { Icon } from '@fluentui/react/lib/Icon';
 
-const subCategoryHeaderStyle = mergeStyles(
+const subCategoryHeaderContainer = mergeStyles(
     {
-      fontSize: '1rem',
-      color: '#212529',
-      fontWeight: 'bold',
-      cursor: 'pointer',
+        display: "flex",
+        alignItems: "center",
+        margin: '10px 0'
+    }
+);
+
+const subCategoryHeaderIcon = mergeStyles(
+    {
+        fontSize: '0.75rem',
+        display: 'flex',
+        aligntItems: 'center',
+    }
+);
+
+const subCategoryHeader = mergeStyles(
+    {
+        display: 'flex',
+        alignItems: 'center',
+        fontSize: '1rem',
+        color: '#212529',
+        fontWeight: 'bold',
+        cursor: 'pointer',
     }
 );
 
@@ -31,72 +49,44 @@ export const CustomizationEntrySubCategory = (props: CustomizationEntrySubCatego
 
     useEffect(() => {
         setIsCollapsed(true); // collapses subCategory headers
-    }, [subCategory]);        // when new Category is selected
+    }, [subCategory]);        // when new Category is selected, subcategory changes
+    
 
-    const onRenderHeader = (props?: IGroupHeaderProps): JSX.Element | null => {
-        if (props) {
-          const toggleCollapse = (): void => {
-            props.onToggleCollapse!(props.group!);
-            setIsCollapsed(!Collapse);
-          };
-          return (
+    const getStyleOptionValue = (key: any) => {
+        return prop(styleOptions, key);
+    };
+
+    const prop = <T, K extends keyof T>(obj: T, key: K) => {
+        return obj[key];
+    }
+    
+    return (
+        <div>
             <div
-                className={subCategoryHeaderStyle}
-                onClick={toggleCollapse}
+                onClick={()=> {setIsCollapsed(!isCollapsed)}}
+                className={subCategoryHeaderContainer}
             >
-                <span style={props.group!.isCollapsed ? {transform: 'rotate(90deg)'} : {}}>
-                    <svg width="11" height="11" viewBox="0 0 6 11" fill="none" xmlns="http://www.w3.org/2000/svg" transform={props.group!.isCollapsed ? '' : 'rotate(90)'} >
-                        <path
-                            fill-rule="evenodd"
-                            clip-rule="evenodd"
-                            d="M4.10274 5.50003L0.5 1.31213L1.19863 0.500031L5.5 5.50003L1.19863 10.5L0.5 9.68793L4.10274 5.50003Z"
-                            fill="black"
-                            stroke="black"
-                            stroke-width="0.8"
-                        />
-                    </svg>                  
+                <span className={subCategoryHeaderIcon}>
+                    <Icon iconName={isCollapsed ? "ChevronRightSmall" : "ChevronDownSmall"}/>
                 </span>
-                <span>
-                    {` ${subCategory}`} 
+                <span className={subCategoryHeader}>
+                    {subCategory}
                 </span>
             </div>
-          );
-        }
-      
-        return null;
-    };
-
-    const onRenderCell = (nestingDepth?: number, item?: CustomizationEntry, itemIndex?: number): React.ReactNode => {
-        
-        const getStyleOptionValue = (key: any) => {
-            return prop(styleOptions, key);
-        };
-
-        function prop<T, K extends keyof T>(obj: T, key: K) {
-            return obj[key];
-        }
-        
-        return item ? (
-            <CustomizationEntrySelector
-                entry={item}
-                value={getStyleOptionValue(item.id)}
-                onChange={updateStyleElement}
-            />
-        ) : null;
-      };
-      
-    const groupedListProps = {
-        onRenderHeader,
-    };
-    const subCategoryGroup: IGroup = {
-        count: entries.length,
-        key: subCategory,
-        name: subCategory,
-        startIndex: 0,
-        data: entries,
-        isCollapsed: isCollapsed,
-    }
-    return (
-        <GroupedList items={entries} onRenderCell={onRenderCell} groupProps={groupedListProps} groups={[subCategoryGroup]} />
+            {
+                isCollapsed
+                    ?   <div />
+                    :   <div>
+                            {entries.map((entry: CustomizationEntry) => (
+                                    <CustomizationEntrySelector
+                                        entry={entry}
+                                        value={getStyleOptionValue(entry.id)}
+                                        onChange={updateStyleElement}
+                                    />
+                                )
+                            )}
+                        </div>
+            }
+        </div>
     );
 };
