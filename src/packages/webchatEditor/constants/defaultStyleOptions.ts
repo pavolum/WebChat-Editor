e40f08a12e3af835c150a4de7ed9cb1733fa6853
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-10-03 17:21:04 -0700

    Changed Style defaults to require hex color

--- src/packages/webchatEditor/constants/defaultStyleOptions.ts
+++ src/packages/webchatEditor/constants/defaultStyleOptions.ts
@@ -3,13 +3,13 @@ function fontFamily(fonts: any) {
   }
   
   const DEFAULT_ACCENT = '#0063B1';
-  const DEFAULT_SUBTLE = '#767676'; // With contrast 4.5:1 to white
+  const DEFAULT_SUBTLE = '#767676'; // With contrast 4.5:1 to #ffffff
   const PADDING_REGULAR = 10;
   
 export const defaultStyleOptions = {
     // Color and paddings
     accent: DEFAULT_ACCENT,
-    backgroundColor: 'White',
+    backgroundColor: '#ffffff',
     cardEmphasisBackgroundColor: '#F0F0F0',
     paddingRegular: PADDING_REGULAR,
     paddingWide: PADDING_REGULAR * 2,
@@ -35,26 +35,26 @@ export const defaultStyleOptions = {
   
     // Bubble
     // TODO: Should we make a bubbleFromBot*
-    bubbleBackground: 'White',
+    bubbleBackground: '#ffffff',
     bubbleBorderColor: '#E6E6E6',
     bubbleBorderRadius: 2,
     bubbleBorderStyle: 'solid',
     bubbleBorderWidth: 1,
-    bubbleFromUserBackground: 'White',
+    bubbleFromUserBackground: '#ffffff',
     bubbleFromUserBorderColor: '#E6E6E6',
     bubbleFromUserBorderRadius: 2,
     bubbleFromUserBorderStyle: 'solid',
     bubbleFromUserBorderWidth: 1,
     bubbleFromUserNubOffset: 0,
     bubbleFromUserNubSize: 0,
-    bubbleFromUserTextColor: 'Black',
+    bubbleFromUserTextColor: '#000000',
     bubbleImageHeight: 240,
     bubbleMaxWidth: 480, // screen width = 600px
     bubbleMinHeight: 40,
     bubbleMinWidth: 250, // min screen width = 300px, Edge requires 372px (https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/13621468/)
     bubbleNubOffset: 'bottom',
     bubbleNubSize: 0,
-    bubbleTextColor: 'Black',
+    bubbleTextColor: '#000000',
   
     // Markdown
     markdownRespectCRLF: true,
@@ -74,7 +74,7 @@ export const defaultStyleOptions = {
     hideSendBox: false,
     hideUploadButton: false,
     microphoneButtonColorOnDictate: '#F33',
-    sendBoxBackground: 'White',
+    sendBoxBackground: '#ffffff',
     sendBoxButtonColor: undefined, // defaults to subtle
     sendBoxButtonColorOnDisabled: '#CCC',
     sendBoxButtonColorOnFocus: '#333',
@@ -82,7 +82,7 @@ export const defaultStyleOptions = {
     sendBoxDisabledTextColor: undefined, // defaults to subtle
     sendBoxHeight: 40,
     sendBoxMaxHeight: 200,
-    sendBoxTextColor: 'Black',
+    sendBoxTextColor: '#000000',
     // TODO: We should deprecate this because there isn't an easy way to make the width of the send box, narrower than the transcript
     sendBoxBorderBottom: '',
     sendBoxBorderLeft: '',
@@ -95,7 +95,7 @@ export const defaultStyleOptions = {
     showSpokenText: false,
   
     // Suggested actions
-    suggestedActionBackground: 'White',
+    suggestedActionBackground: '#ffffff',
     suggestedActionBorder: undefined, // split into 3, null
     suggestedActionBorderColor: undefined, // defaults to accent
     suggestedActionBorderRadius: 0,
@@ -128,7 +128,7 @@ export const defaultStyleOptions = {
     transcriptOverlayButtonBackground: 'rgba(0, 0, 0, .6)',
     transcriptOverlayButtonBackgroundOnFocus: 'rgba(0, 0, 0, .8)',
     transcriptOverlayButtonBackgroundOnHover: 'rgba(0, 0, 0, .8)',
-    transcriptOverlayButtonColor: 'White',
+    transcriptOverlayButtonColor: '#ffffff',
     transcriptOverlayButtonColorOnFocus: undefined, // defaults to transcriptOverlayButtonColor
     transcriptOverlayButtonColorOnHover: undefined, // defaults to transcriptOverlayButtonColor
   
